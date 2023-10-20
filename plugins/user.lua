694a6bbc242be537c3046e8042e9f0b47bdf6483
Benjamin Rottler <benjamin.rottler@cern.ch>
2023-10-20 12:37:18 +0200

    Fix whitespace

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,7 +1,7 @@
 return {
   { "tpope/vim-fugitive", lazy = false },
   { "tpope/vim-rhubarb", lazy = false },
-  { "shumphrey/fugitive-gitlab.vim", lazy = false, 
+  { "shumphrey/fugitive-gitlab.vim", lazy = false,
     config = function()
       vim.g.fugitive_gitlab_domains = {"https://gitlab.cern.ch/", "https://git.bfg.privat/"}
     end
