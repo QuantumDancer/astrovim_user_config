fbdea30fb4f0402152c8ee33fa3e132902e6e37d
Benjamin Rottler <benjamin.rottler@cern.ch>
2024-02-16 15:34:31 +0100

    Add barbecue plugin

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,18 +1,22 @@
 return {
   { "tpope/vim-fugitive", lazy = false },
   { "tpope/vim-rhubarb", lazy = false },
-  { "shumphrey/fugitive-gitlab.vim", lazy = false,
-    config = function()
-      vim.g.fugitive_gitlab_domains = {"https://gitlab.cern.ch/", "https://git.bfg.privat/"}
-    end
+  {
+    "shumphrey/fugitive-gitlab.vim",
+    lazy = false,
+    config = function() vim.g.fugitive_gitlab_domains = { "https://gitlab.cern.ch/", "https://git.bfg.privat/" } end,
   },
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+  {
+    "utilyre/barbecue.nvim",
+    name = "barbecue",
+    version = "*",
+    dependencies = {
+      "catppuccin",
+      "SmiteshP/nvim-navic",
+      "nvim-tree/nvim-web-devicons", -- optional dependency
+    },
+    opts = {
+      -- configurations go here
+    },
+  },
 }
